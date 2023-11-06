689fcc9404bbcb10989e6342d016a5c361344552
Connor Slade <connor@connorcode.com>
2023-11-06 15:56:41 -0500

    Multiple citations at once

--- src/features/citationGenerator.ts
+++ src/features/citationGenerator.ts
@@ -1,3 +1,4 @@
+import { Selection, TextEditor } from "vscode";
 import * as vscode from "vscode";
 
 import axios from "axios";
@@ -8,52 +9,74 @@ export function register(context: vscode.ExtensionContext) {
       "note-utils.citationGenerator",
       async () => {
         const editor = vscode.window.activeTextEditor;
-
         if (!editor) return err("No active editor");
-        let selected = editor.selection;
-        if (selected.isEmpty) return err("No text selected");
+        if (editor.selections.length == 0) return err("No text selected");
 
-        let text = editor.document.getText(selected);
-        let url = `https://www.mybib.com/api/autocite/search?q=${encodeURIComponent(
-          text
-        )}&sourceId=webpage`;
-
-        vscode.window.showInformationMessage("Fetching citation...");
-
-        let siteLookup = await axios
-          .get(url)
-          .catch((error) =>
-            err(`Failed to search for resource citation: ${error}`)
-          );
-        if (!siteLookup) return;
-        let siteInfo = siteLookup.data.results[0].metadata;
-
-        // POST /api/autocite/reference
-        // {
-        //   "metadata": siteInfo,
-        //   "sourceId": "webpage",
-        //   "styleId": "modern-language-association-8th-edition"
-        // }
-        let citation = await axios
-          .post("https://www.mybib.com/api/autocite/reference", {
-            metadata: siteInfo,
-            sourceId: "webpage",
-            styleId: "modern-language-association-8th-edition",
-          })
-          .catch((error) => err(`Failed to generate citation: ${error}`));
-        if (!citation) return;
-
-        let formattedCitation = citation.data.result.formattedReferenceStr;
-        let textCitation = formattedCitation
-          .replace(/<[^>]*>?/gm, "")
-          .replace(/“”/gm, '"');
-
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selected, textCitation);
-        });
+        for (let selection of editor.selections)
+          new CitationJob(editor, selection)
+            .cite()
+            .then((job) => {
+              if (job.citation === null) return;
+              editor.edit((editBuilder) => {
+                editBuilder.replace(job.selection, job.citation!);
+              });
+            })
+            .catch((error) => err(`Failed to generate citation: ${error}`));
       }
     )
   );
+}
+
+class CitationJob {
+  editor: TextEditor;
+  selection: Selection;
+  address: string;
+  citation: string | null;
+
+  constructor(editor: TextEditor, selection: Selection) {
+    this.editor = editor;
+    this.selection = selection;
+    this.address = editor.document.getText(selection);
+    this.citation = null;
+  }
+
+  async cite(): Promise<CitationJob> {
+    let url = `https://www.mybib.com/api/autocite/search?q=${encodeURIComponent(
+      this.address
+    )}&sourceId=webpage`;
+
+    vscode.window.showInformationMessage("Fetching citation...");
+
+    let siteLookup = await axios
+      .get(url)
+      .catch((error) =>
+        err(`Failed to search for resource citation: ${error}`)
+      );
+    if (!siteLookup) return this;
+    let siteInfo = siteLookup.data.results[0].metadata;
+
+    // POST /api/autocite/reference
+    // {
+    //   "metadata": siteInfo,
+    //   "sourceId": "webpage",
+    //   "styleId": "modern-language-association-8th-edition"
+    // }
+    let citation = await axios
+      .post("https://www.mybib.com/api/autocite/reference", {
+        metadata: siteInfo,
+        sourceId: "webpage",
+        styleId: "modern-language-association-8th-edition",
+      })
+      .catch((error) => err(`Failed to generate citation: ${error}`));
+    if (!citation) return this;
+
+    let formattedCitation = citation.data.result.formattedReferenceStr;
+    this.citation = formattedCitation
+      .replace(/<[^>]*>?/gm, "")
+      .replace(/[“”]/gm, '"');
+
+    return this;
+  }
 }
 
 function err(msg: string) {
